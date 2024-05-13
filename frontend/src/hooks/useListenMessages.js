299b4b6240181cbe9515fb360e9d00d07e33dba8
Skitarii11 <jawhaa_1107@yahoo.com>
2024-05-13 15:55:21 +0800

    deployment

---
+++ frontend/src/hooks/useListenMessages.js
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+
+import { useSocketContext } from "../context/SocketContext";
+import useConversation from "../zustand/useConversation";
+
+import notificationSound from "../assets/sounds/notification.mp3";
+
+const useListenMessages = () => {
+	const { socket } = useSocketContext();
+	const { messages, setMessages } = useConversation();
+
+	useEffect(() => {
+		socket?.on("newMessage", (newMessage) => {
+			newMessage.shouldShake = true;
+			const sound = new Audio(notificationSound);
+			sound.play();
+			if (check) {
+				setMessages([...messages, newMessage]);
+			}
+		});
+
+		return () => {
+			socket?.off("new-message");
+		};
+	}, [socket, setMessages, messages]);
+};
+export default useListenMessages;
